8dacb16664827110448085fa971cbb93fbe9a0a8
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-05-22 22:37:18 +0200

    Added bingo setup page

---
+++ routes/bingo-css.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+module.exports = {
+	async execute(req, res) {
+		// Send back an HTML file
+		res.setHeader('Content-Type', 'text/css');
+		res.end(fs.readFileSync('./css/bingo-setup.css'));
+	}
+};
--- routes/bingo.js
+++ routes/bingo.js
@@ -2,6 +2,16 @@ const fs = require('fs');
 
 module.exports = {
 	async execute(req, res) {
+		const urlParams = new URLSearchParams(req.url.split('?')[1]);
+		const userId = urlParams.get('u');
+
+		if (!userId || isNaN(userId)) {
+			// Send back an HTML file
+			res.setHeader('Content-Type', 'text/html');
+			res.end(fs.readFileSync('./webpages/bingo-setup.html'));
+			return;
+		}
+
 		// Send back an HTML file
 		res.setHeader('Content-Type', 'text/html');
 		res.end(fs.readFileSync('./webpages/bingo.html'));
