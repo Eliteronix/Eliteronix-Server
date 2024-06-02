52df6049f0c972b5181a2ceb6d584359837c2aa2
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-06-02 21:55:49 +0200

    Started adding matchoverlay

---
+++ routes/matchoverlay.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+module.exports = {
+	async execute(req, res) {
+		const urlParams = new URLSearchParams(req.url.split('?')[1]);
+		const userId = urlParams.get('u');
+
+		if (!userId || isNaN(userId)) {
+			// Send back an HTML file
+			res.setHeader('Content-Type', 'text/html');
+			res.end(fs.readFileSync('./webpages/matchoverlay.html'));
+			return;
+		}
+
+		// Send back an HTML file
+		res.setHeader('Content-Type', 'text/html');
+		res.end(fs.readFileSync('./webpages/matchoverlay.html'));
+	}
+};
