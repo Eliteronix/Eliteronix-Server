8a7be0f1c33dd3d3c3e2112130ed67228b21d0e6
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-09-09 02:05:49 +0200

    Added done endpoint

--- dbObjects.js
+++ dbObjects.js
@@ -15,8 +15,43 @@ const multiGameScores = new Sequelize('database', 'username', 'password', {
 	}
 });
 
+const gamifyProcessQueue = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: `${process.env.ELITEBOTIXROOTPATH}/databases/processQueue.sqlite`,
+	retry: {
+		max: 25, // Maximum retry 15 times
+		backoffBase: 100, // Initial backoff duration in ms. Default: 100,
+		backoffExponent: 1.14, // Exponent to increase backoff each try. Default: 1.1
+	},
+	pool: {
+		max: 7,
+	}
+});
+
+const gamifyMainData = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: `${process.env.ELITEBOTIXROOTPATH}/databases/mainData.sqlite`,
+	retry: {
+		max: 25, // Maximum retry 15 times
+		backoffBase: 100, // Initial backoff duration in ms. Default: 100,
+		backoffExponent: 1.14, // Exponent to increase backoff each try. Default: 1.1
+	},
+	pool: {
+		max: 7,
+	}
+});
+
 const DBOsuMultiGameScores = require(`${process.env.ELITEBOTIXROOTPATH}/models/DBOsuMultiGameScores`)(multiGameScores, Sequelize.DataTypes);
 
+const DBGamifyProcessQueue = require(`${process.env.GAMIFYROOTPATH}/models/DBProcessQueue`)(gamifyProcessQueue, Sequelize.DataTypes);
+const DBGamifyTasks = require(`${process.env.GAMIFYROOTPATH}/models/DBTasks`)(gamifyMainData, Sequelize.DataTypes);
+
 module.exports = {
-	DBOsuMultiGameScores
+	DBOsuMultiGameScores,
+	DBGamifyProcessQueue,
+	DBGamifyTasks,
 };
---
+++ routes/gamify-done.js
@@ -0,0 +1,47 @@
+const { DBGamifyTasks, DBGamifyProcessQueue } = require('../dbObjects');
+
+module.exports = {
+	async execute(req, res) {
+		const urlParams = new URLSearchParams(req.url.split('?')[1]);
+		const guildId = urlParams.get('g');
+
+		if (!guildId || isNaN(guildId)) {
+			res.setHeader('Content-Type', 'text/plain');
+			res.end('Invalid guildId');
+			return;
+		}
+
+		const task = urlParams.get('t');
+
+		if (!task) {
+			res.setHeader('Content-Type', 'text/plain');
+			res.end('Invalid task');
+			return;
+		}
+
+		// Get the task from the database
+		const taskEntry = await DBGamifyTasks.findOne({
+			where: {
+				guildId: guildId,
+				name: task
+			}
+		});
+
+		if (!taskEntry) {
+			res.setHeader('Content-Type', 'text/plain');
+			res.end('Task not found');
+			return;
+		}
+
+		// Update task status and trigger update
+		await DBGamifyProcessQueue.create({
+			guildId: guildId,
+			task: 'setDone',
+			additions: taskEntry.id,
+			date: new Date()
+		});
+
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('updated task status');
+	}
+};
