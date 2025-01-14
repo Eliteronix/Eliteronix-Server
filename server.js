976494576f9842972a9de5f50e23d23d5db4d484
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-01-14 22:36:48 +0100

    added back error logging

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const browserSourceServer = http.createServer(async (req, res) => {
 		});
 	} catch (error) {
 		if (error.code === 'MODULE_NOT_FOUND') {
-			// console.error('Route not found:', route);
+			console.error('Route not found:', route);
 		} else {
 			console.error(route, error);
 		}
