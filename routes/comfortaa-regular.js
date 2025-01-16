91c151c8f16afada605fdb0c343d9b4189c24f81
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-01-16 04:39:33 +0100

    Update comfortaa-regular.js

--- routes/comfortaa-regular.js
+++ routes/comfortaa-regular.js
@@ -4,6 +4,8 @@ module.exports = {
 	async execute(req, res) {
 
 		// Send the font from the assets folder
+		// allow cors
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Content-Type', 'font/ttf');
 		res.end(fs.readFileSync('./assets/Comfortaa-Regular.ttf'));
 	}
