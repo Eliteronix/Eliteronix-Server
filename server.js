0ee66bfdf6b52552f7084b59d1936433e5f0180c
Marvin Zimmermann <zimmermann.mariomarvin@gmail.com>
2025-06-25 23:05:06 +0200

    Added greenlock codebase

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 require('dotenv').config();
+const Greenlock = require('greenlock');
 
 // Define the HTTP server
 const browserSourceServer = http.createServer(async (req, res) => {
@@ -35,10 +36,37 @@ const browserSourceServer = http.createServer(async (req, res) => {
 });
 
 // eslint-disable-next-line no-console
-console.log('Server running on port 80');
+//console.log('Server running on port 80');
 
 // Start the HTTP server which exposes the browsersources on http://localhost:80/duelRating/1234
-browserSourceServer.listen(80);
+//browserSourceServer.listen(80);
 
 // Start the HTTPS server which exposes the browsersources on https://localhost:443/duelRating/1234
 // browserSourceServer.listen(443);
+
+// Greenlock setup
+Greenlock.create({
+	packageRoot: __dirname,
+	configDir: './greenlock.d',
+	maintainerEmail: 'you@example.com',
+	cluster: false,
+	agreeToTerms: true,
+	sites: [
+		{
+			subject: 'yourdomain.com',
+			altnames: ['yourdomain.com', 'www.yourdomain.com']
+		}
+	]
+}).ready(glx => {
+	// Start HTTP server (needed for Let's Encrypt challenge)
+	http.createServer(glx.middleware(requestHandler)).listen(80, () => {
+		console.log('HTTP server running on port 80');
+	});
+
+	// Start HTTPS server
+	require('https')
+		.createServer(glx.httpsOptions, glx.middleware(requestHandler))
+		.listen(443, () => {
+			console.log('HTTPS server running on port 443');
+		});
+});
