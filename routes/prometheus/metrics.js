bf4dd24d92a5fcfa696c40124d655c1d68ed3c04
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2025-11-07 00:50:57 +0000

    Update metrics.js

--- routes/prometheus/metrics.js
+++ routes/prometheus/metrics.js
@@ -1,7 +1,8 @@
 module.exports = {
 	async execute(req, res) {
 		const response = await fetch('http://localhost:9090/metrics');
+		const body = await res.text();
 
-		console.log(response);
+		console.log(body);
 	}
 };
